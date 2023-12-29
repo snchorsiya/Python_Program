04f8f8ee008d3fabb1149844873de99d8642ebd2
Sheetal <Sheetal.Chorsiya@Mantra.Local>
2023-12-29 11:58:01 +0530

    This is a creation html report and logging

---
+++ PyTestDemo/test_logging.py
@@ -0,0 +1,19 @@
+import logging
+
+
+def test_logging():
+
+    logger = logging.getLogger(__name__)
+
+    fileHandler = logging.FileHandler('logfile.log')
+    formatter = logging.Formatter("%(asctime)s :%(levelname)s :%(name)s :%(message)s")
+    fileHandler.setFormatter(formatter)
+
+    logger.addHandler(fileHandler)  # filehandler object
+
+    logger.setLevel(logging.ERROR)
+    logger.debug("A debug statement is executed")
+    logger.info("Information statement")
+    logger.warning("Something is in warning mode")
+    logger.error("A major error has happend")
+    logger.critical("Critical issue")
